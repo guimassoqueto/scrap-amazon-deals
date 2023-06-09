688064af2c1d209e6822644d52862ed950e404e3
guimassoqueto <gui.massoqueto@gmail.com>
2023-06-09 10:54:32 -0300

    refactor: collect book name from anchor attribute

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,9 +10,17 @@ class BookspiderSpider(scrapy.Spider):
         books = response.css("article.product_pod")
         for book in books:
             yield {
-                "name": book.css("article>h3>a::text").get(),
+                "name": book.css("article>h3>a::attr(title)").get(),
                 "price": book.css(
                     "article>div.product_price>p.price_color::text"
                 ).get(),
                 "url": book.css("article>h3>a").attrib["href"],
             }
+
+        next_page: str = response.css("div>ul.pager>li.next>a::attr(href)").get()
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+            yield response.follow(next_page_url, callback=self.parse)
