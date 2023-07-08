ebbdf76431ff88dbdfb533da1c8c12a37186fb5c
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-08 13:44:14 -0300

    feat: update pipelines

--- amazon/amazon/pipelines.py
+++ amazon/amazon/pipelines.py
@@ -5,13 +5,20 @@
 
 
 # useful for handling different item types with a single interface
+from typing import List, Tuple
 from itemadapter import ItemAdapter
-import psycopg2
-from amazon.helpers.postgres_helper import upsert_query
+from amazon.helpers.postgres_service import PostgresDB
+from amazon.helpers.generate_pid_errors_file import generate_pid_errors_file
 from amazon.items import ProductFields
 from re import search, sub
-from amazon.settings import POSTGRES_DB, POSTGRES_HOST, POSTGRES_PASSWORD, POSTGRES_USER
 from logging import getLogger
+from amazon.settings import (
+    POSTGRES_DB,
+    POSTGRES_HOST,
+    POSTGRES_PASSWORD,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+)
 
 logger = getLogger("pipelines.py")
 
@@ -24,7 +31,8 @@ class PlaywrightAmazonPipeline:
         field_names = adapter.field_names()
         for field_name in field_names:
             value = adapter.get(field_name)
-            adapter[field_name] = value.strip().replace("'", "")
+            if isinstance(value, str):
+                adapter[field_name] = sub(r"[^a-zA-Zà-úÀ-Ú0-9_\s\.>]", "", value)
 
         # convert review count to integer
         value = adapter.get(ProductFields.REVIEWS.value)
@@ -36,27 +44,31 @@ class PlaywrightAmazonPipeline:
 
 
 class SaveToPostgresPipeline:
-    def __init__(self):
-        ## Create/Connect to database
-        self.connection = psycopg2.connect(
-            host=POSTGRES_HOST,
-            user=POSTGRES_USER,
-            password=POSTGRES_PASSWORD,
-            dbname=POSTGRES_DB,
+    async def process_item(self, item, spider):
+        pg = PostgresDB(
+            POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT
         )
-
-        ## Create cursor, used to execute commands
-        self.cur = self.connection.cursor()
-
-    def process_item(self, item, spider):
-        ## Define insert statement
-        self.cur.execute(upsert_query("products", item))
-
-        ## Execute insert of data into database
-        self.connection.commit()
+        await pg.upsert_item("products", item)
         return item
 
+
+class WritePidErrorsPipeline:
     def close_spider(self, spider):
-        ## Close cursor & connection to database
-        self.cur.close()
-        self.connection.close()
+        pid_errors_file = generate_pid_errors_file("logs.log")
+        pg = PostgresDB(
+            POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_PORT
+        )
+        non_inserted_ids = pg.select_non_inserted_ids(pid_errors_file)
+        if non_inserted_ids:
+            self.write_non_inserted_ids_file(non_inserted_ids)
+        else:
+            logger.error("[SUCCESS]: The database is not missing any product")
+
+    def write_non_inserted_ids_file(
+        self,
+        non_inserted_ids: List[Tuple[str]],
+        output_file: str = "/home/gmassoqueto/github-repos/scraper-beautiful-soup/insertion_errors.log",
+    ) -> None:
+        with open(output_file, "w", encoding="utf-8") as f:
+            for non_inserted_id in non_inserted_ids:
+                f.write(f"{non_inserted_id[0]}\n")
