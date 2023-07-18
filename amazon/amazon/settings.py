4aa419c5c52642b9833ce98165a804708929d443
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-18 14:33:49 -0300

    feat: add new enviroment variables

--- amazon/amazon/settings.py
+++ amazon/amazon/settings.py
@@ -28,6 +28,11 @@ POSTGRES_DB = getenv("POSTGRES_DB") or "postgres"
 POSTGRES_USER = getenv("POSTGRES_USER") or "postgres"
 POSTGRES_PASSWORD = getenv("POSTGRES_PASSWORD") or "password"
 POSTGRES_HOST = getenv("POSTGRES_HOST") or "127.0.0.1"
+
+RABBITMQ_DEFAULT_USER = getenv("RABBITMQ_DEFAULT_USER") or "user"
+RABBITMQ_DEFAULT_PASS = getenv("RABBITMQ_DEFAULT_PASS") or "password"
+RABBITMQ_DEFAULT_HOST = getenv("RABBITMQ_DEFAULT_HOST") or "localhost"
+RABBITMQ_MAIN_QUEUE = getenv("RABBITMQ_MAIN_QUEUE") or "scrapy-soup"
 # end .env variables
 
 
