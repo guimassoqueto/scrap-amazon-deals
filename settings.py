8535e73e22d3c97162b365cced1c6b8def14b126
guimassoqueto <gui.massoqueto@gmail.com>
2023-07-21 13:13:11 -0300

    feat: add new environment variable

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from os import getenv
 
+load_dotenv()
 
 POSTGRES_PORT = getenv("POSTGRES_PORT") or 5432
 POSTGRES_DB = getenv("POSTGRES_DB") or "postgres"
@@ -12,3 +13,5 @@ RABBITMQ_DEFAULT_USER = getenv("RABBITMQ_DEFAULT_USER") or "user"
 RABBITMQ_DEFAULT_PASS = getenv("RABBITMQ_DEFAULT_PASS") or "password"
 RABBITMQ_DEFAULT_HOST = getenv("RABBITMQ_DEFAULT_HOST") or "localhost"
 RABBITMQ_MAIN_QUEUE = getenv("RABBITMQ_MAIN_QUEUE") or "scrapy-soup"
+
+DEALS_PAGES = getenv("DEALS_PAGES") or 1
