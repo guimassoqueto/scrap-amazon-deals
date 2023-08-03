dcebbfb673b2ed9b984e9f1b50a97cc8e41038ef
guimassoqueto <gui.massoqueto@gmail.com>
2023-08-03 07:40:50 -0300

    feat: add new environment variable

--- amazon/amazon/settings.py
+++ amazon/amazon/settings.py
@@ -40,7 +40,7 @@ NEWSPIDER_MODULE = "amazon.spiders"
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 16
+CONCURRENT_REQUESTS = int(getenv("MAX_CONCURRENCY")) or 16
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
